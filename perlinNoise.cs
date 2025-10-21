8a5d468277dec560410fc3788d63bc80dcc34962
Aaryan-Pala <158114672+Aaryan-Pala@users.noreply.github.com>
2025-10-21 20:41:49 +1100

    Update perlinNoise.cs
    added gradient func

--- perlinNoise.cs
+++ perlinNoise.cs
@@ -26,6 +26,32 @@ public class perlinNoise {
         return x - Math.floor(x);
     }
 
+    // Find gradient vectors for cells
+    public static double grad(int hash, double x, double y, double z)
+    {
+        switch(hash & 0xF)
+        {
+            case 0x0: return  x + y;
+            case 0x1: return -x + y;
+            case 0x2: return  x - y;
+            case 0x3: return -x - y;
+            case 0x4: return  x + z;
+            case 0x5: return -x + z;
+            case 0x6: return  x - z;
+            case 0x7: return -x - z;
+            case 0x8: return  y + z;
+            case 0x9: return -y + z;
+            case 0xA: return  y - z;
+            case 0xB: return -y - z;
+            case 0xC: return  y + x;
+            case 0xD: return -y + z;
+            case 0xE: return  y - x;
+            case 0xF: return -y - z;
+            default: return 0; // never happens
+        }
+    }
+
+
     // shuffles permutation based on seed
     private static void shuffle(int[] arr, int seed)
     {
